7721987b7781c0542cc313065724c68c1da230d9
bishalkarki01 <63300213+bishalkarki01@users.noreply.github.com>
2024-04-17 22:10:36 -0500

    dfds

--- hw4/springrolls/src/main/java/com/cs502/hw4/repository/RecipeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cs502.hw4.repository;
-
-import com.cs502.hw4.model.Recipe;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RecipeRepository extends MongoRepository<Recipe, String> {
-  
-}
